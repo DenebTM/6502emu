15e6e3abb36fbd5553b025f264876ceb5f0c9f3e
DenebTM <deneb@screee.ee>
2024-01-08 15:06:14 +0100

    improve fbclear.asm

--- roms/fbclear.asm
+++ roms/fbclear.asm
@@ -1,37 +1,60 @@
-start    = $ff00
-fbadd_lo = $00
-fbadd_hi = $90
-fbpage   = $9a00
-addr     = $00
-addr_hi  = $01
-addr_offset = $02
+start       = $ff00 ; ROM start address
 
+FB_BANK_CNT = 15    ; number of framebuffer-internal screen memory banks
+FB_PAGE_CNT = 10    ; number of memory pages taken up by framebuffer MMIO
+FBADD_LO    = $00   ; framebuffer MMIO screen memory address (low byte)
+FBADD_HI    = $90   ; framebuffer MMIO screen memory address (high byte)
+FB_BANK_REG = $9a00 ; framebuffer MMIO address to set current bank (0..(FB_BANK_CNT-1))
+
+addr        = $00   ; address holding low byte of framebuffer address
+addr_hi     = $01   ; address holding high byte of framebuffer address
+addr_offset = $02   ; address holding current framebuffer high byte offset (0..(FB_PAGE_CNT-1))
+pattern     = $03   ; address holding 8px pattern to be written to framebuffer
+; row_remain  = $04   ; address holding number of bytes remaining in current row
+
+; setup reset and interrupt vectors
 *=$fffc
 .word start
 .word start
 
-*=start
-lda #10
-sta addr_offset
+*=start             
+lda #$ff            ; initialize value to $ff (8 pixels of white)
+; lda #$aa            ; initialize value to $aa (4+4 alternating black/white pixels)
+sta pattern
+; lda #80
+; sta row_remain
 
-ldx #16
+restart:            ; jump here after first run
+ldx #FB_BANK_CNT    ; start with highest memory bank used by screen memory
 L1:
   dex
-  stx fbpage
+  stx FB_BANK_REG
   php
 
-  lda #10
+  lda #FB_PAGE_CNT
   sta addr_offset
   L2:
     dec addr_offset
     php
-    jsr set_addr
-  
-    lda #$ff
+    jsr sub_set_addr
+
+    lda pattern
     ldy #$00
     L3:
       dey
       sta (addr),y
+
+    ; uncomment to invert the pattern after every scanline
+    ;   php
+    ;   dec row_remain
+    ;   bne L3_end
+    ; flip_row:
+    ;   lda #80
+    ;   sta row_remain
+    ;   jsr sub_invert
+    ;   lda pattern
+    ;  plp
+
     L3_end:
       bne L3
 
@@ -44,12 +67,20 @@ L1_end:
   bne L1
 
 end:
-  jmp end
+; jmp end
+jsr sub_invert      ; invert pattern after every pass
+jmp restart
 
-set_addr:
-  lda #fbadd_lo
+sub_invert:         ; flip all bits in pattern
+  lda pattern
+  eor #$ff
+  sta pattern
+  rts
+
+sub_set_addr:       ; set base address to write to ($9000..$9900)
+  lda #FBADD_LO
   sta addr
-  lda #fbadd_hi
+  lda #FBADD_HI
   clc
   adc addr_offset
   sta addr_hi
