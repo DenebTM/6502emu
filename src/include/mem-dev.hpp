886200e7e2cdae25dd53ca82ca5893689429d9b9
DenebTM <deneb@screee.ee>
2024-01-16 21:53:53 +0100

    make mem-dev abstract again

--- src/include/mem-dev.hpp
+++ src/include/mem-dev.hpp
@@ -3,11 +3,17 @@
 
 #include "emu-types.hpp"
 
+/**
+ * abstract base class representing a peripheral device accessible via MMIO
+ */
 struct MemoryMappedDevice {
+protected:
   MemoryMappedDevice(bool ro, Word mapped_regs_count)
       : read_only(ro), mapped_regs_count(mapped_regs_count), mapped_regs(new Byte[mapped_regs_count]) {
     std::fill_n(mapped_regs, mapped_regs_count, 0xff);
   }
+
+public:
   virtual ~MemoryMappedDevice() { delete[] mapped_regs; }
 
   const bool read_only = false;
